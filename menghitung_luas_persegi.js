6c1d7f324e9c3724a81dce68e4be867e41ec6589
tabir123 <86651458+tabir123@users.noreply.github.com>
2021-07-26 13:07:57 +0700

    Delete menghitung_luas_persegi.js

--- menghitung_luas_persegi.js
+++
@@ -1,7 +0,0 @@
-var panjang = prompt('masukan nilai panjangr');
-var lebar  = prompt('masukan nilai lebar');
-    panjang = parseInt(panjang);
-    lebar = parseInt(lebar);
-var luas;
-    luas = panjang * lebar;
-    document.write('luas panjang adalah =' + luas);
