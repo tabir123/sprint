fc444ffac3967741d196f87d075c6e2cdd1e19e5
tabir123 <86651458+tabir123@users.noreply.github.com>
2021-07-26 14:00:01 +0700

    Add files via upload

---
+++ menghitung_luas_persegi.js
@@ -0,0 +1,12 @@
+function MenghitungLuasPersegiPanajang(panjang, lebar) {
+luas = panjang * lebar
+
+document.write('panjang :' + panjang + '<br>');
+document.write('lebar :' + lebar + '<br>');
+document.write('luas persegi panjang :' + luas )
+
+}
+panjang = prompt('masukan nilai panjang' );
+lebar = prompt('masukan nilai lebar');
+
+MenghitungLuasPersegiPanajang(panjang, lebar)
