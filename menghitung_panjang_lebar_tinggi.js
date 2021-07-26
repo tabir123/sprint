92dd4aa6194d0ce8a030673bcca57d99c19558cf
tabir123 <86651458+tabir123@users.noreply.github.com>
2021-07-26 12:31:14 +0700

    Add files via upload

---
+++ menghitung_panjang_lebar_tinggi.js
@@ -0,0 +1,13 @@
+var panjang = prompt('masukan nilai panjangr');
+var lebar  = prompt('masukan nilai lebar');
+var tinggi = prompt('masukan nilai tinggi');
+    panjang = parseInt(panjang);
+    lebar = parseInt(lebar);
+    tinggi = parseInt(tinggi);
+var volumebalok;
+    volumebalok  = panjang * lebar * tinggi;
+    document.write('luas volume balok =' + volumebalok + '<br>');
+var luaspermukaanbalok;
+    luaspermukaanbalok = panjang * lebar
+    document.write('luas permukaann balok =' + luaspermukaanbalok);
+    
