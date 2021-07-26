8d1af89d26c8a51faf1a30c82bcd578cb0ff0658
tabir123 <86651458+tabir123@users.noreply.github.com>
2021-07-26 11:43:32 +0700

    Add files via upload

---
+++ menghitung_luas_persegi.js
@@ -0,0 +1,7 @@
+var panjang = prompt('masukan nilai panjangr');
+var lebar  = prompt('masukan nilai lebar');
+    panjang = parseInt(panjang);
+    lebar = parseInt(lebar);
+var luas;
+    luas = panjang * lebar;
+    document.write('luas panjang adalah =' + luas);
